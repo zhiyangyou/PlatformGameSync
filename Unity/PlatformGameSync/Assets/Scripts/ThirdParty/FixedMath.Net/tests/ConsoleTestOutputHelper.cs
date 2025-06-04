412293385bef7272360d76ff75dbcba2fa69d544
youzhiyang <you1454900087@gmail.com>
2025-06-04 09:33:31 +0800

    MINOR: [BEPU] 删除无用XUnit测试用例

--- Unity/PlatformGameSync/Assets/Scripts/ThirdParty/FixedMath.Net/tests/ConsoleTestOutputHelper.cs
+++ Unity/PlatformGameSync/Assets/Scripts/ThirdParty/FixedMath.Net/tests/ConsoleTestOutputHelper.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using Xunit.Abstractions;
-
-namespace FixMath.NET
-{
-	public class ConsoleTestOutputHelper : ITestOutputHelper
-	{
-		public void WriteLine(string message)
-		{
-			Console.WriteLine(message);
-		}
-
-		public void WriteLine(string format, params object[] args)
-		{
-			Console.WriteLine(format, args);
-		}
-	}
-}
+﻿// using System;
+// using Xunit.Abstractions;
+//
+// namespace FixMath.NET
+// {
+// 	public class ConsoleTestOutputHelper : ITestOutputHelper
+// 	{
+// 		public void WriteLine(string message)
+// 		{
+// 			Console.WriteLine(message);
+// 		}
+//
+// 		public void WriteLine(string format, params object[] args)
+// 		{
+// 			Console.WriteLine(format, args);
+// 		}
+// 	}
+// }
