6711767830efd056146eaf0366d8bb96f0904552
youzhiyang <you1454900087@gmail.com>
2025-06-06 16:12:16 +0800

    NEW: 测试Player

--- Unity/PlatformGameSync/Assets/Scripts/BEPU_Adapter/Test/TestPlayer.cs
+++ Unity/PlatformGameSync/Assets/Scripts/BEPU_Adapter/Test/TestPlayer.cs
@@ -5,32 +5,51 @@ using Vector3 = BEPUutilities.Vector3;
 
 
 public class TestPlayer : MonoBehaviour {
-    public BEPU_BaseCollider sphereCollider;
-
+    public BEPU_BaseCollider _collider;
+    public  Animator _animator;
+    public SpriteRenderer _spRender;
     public float moveSpeed = 3f;
     public float jumpForce = 8f;
+    public bool _facingRight = true;
 
-    private float xInput = 0f;
+    private float _xInput = 0f;
+    private bool _isMoving = false;
 
-    private void Awake() { }
+    private void Awake() {
+        _collider = GetComponent<BEPU_BaseCollider>();
+        _animator = GetComponent<Animator>();
+        _spRender = GetComponent<SpriteRenderer>();
+    }
 
+    private void HanldeAnimations() {
+        _isMoving = _collider.entity.LinearVelocity.X != Fix64.Zero;
+        _animator.SetBool("IsMoving", _isMoving);
+    }
+
+    private void HandleMove() {
+        var oldV = _collider.entity.LinearVelocity;
+        oldV.X = (Fix64)_xInput * (Fix64)moveSpeed;
+        _collider.entity.LinearVelocity = oldV;
+    }
+
+    private void HandleJump() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            var oldV = _collider.entity.LinearVelocity;
+            oldV.Y = (Fix64)jumpForce;
+            _collider.entity.LinearVelocity = oldV;
+        }
+    }
+
+    private void HandleFlip() {
+        var isFlip = _collider.entity.LinearVelocity.X < Fix64.Zero;
+        _facingRight = isFlip;
+        _spRender.flipX = _facingRight;
+    }
     private void Update() {
-        xInput = Input.GetAxis("Horizontal");
-
-        // 水平移动
-        {
-            var oldV = sphereCollider.entity.LinearVelocity;
-            oldV.X = (Fix64)xInput * (Fix64)moveSpeed;
-            sphereCollider.entity.LinearVelocity = oldV;
-        }
-
-        // 跳
-        {
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                var oldV = sphereCollider.entity.LinearVelocity;
-                oldV.Y = (Fix64)jumpForce;
-                sphereCollider.entity.LinearVelocity = oldV;
-            }
-        }
+        _xInput = Input.GetAxis("Horizontal");
+        HandleMove();
+        HandleJump();
+        HanldeAnimations();
+        HandleFlip();
     }
 }
